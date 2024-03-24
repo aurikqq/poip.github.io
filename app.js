bd8ba61c6d2ece840d0102972577a163a0ad7cfb
aurikqq <129595806+aurikqq@users.noreply.github.com>
2024-03-24 20:50:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let tg = window.Telegram.WebApp;
 
+console.log('тест1')
+
 document.addEventListener('DOMContentLoaded', function() {
   fetch('https://ipinfo.io/json')
     .then(response => response.json())
     .then(data => {
+      console.log('тест2')
       tg.sendData(data.ip)
+      tg.sendData(user_ip)
       tg.close()
   })
 })
